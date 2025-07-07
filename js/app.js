e9f10cfbe263971e517d8f58a632ebb0f969e1c2
saqlinezaman <saqlinemoaj@gmail.com>
2025-07-07 23:16:17 +0600

    place card section done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
 const allBtn = document.querySelectorAll('.card-btn');
+// set innner text Function
+
+function setInnerText(id,value) {
+    document.getElementById(id).innerText = value;
+}
 
 let count = 0
     for(const btn of allBtn){
         btn.addEventListener('click',function(event){
            count = count + 1 ;
-           document.getElementById('card-count').innerText = count;
-        })
+           const placeName =  event.target.parentNode.parentNode.childNodes[1].innerText;
+           const Price =  event.target.parentNode.parentNode.childNodes[3].innerText;
+
+           document.getElementById('place-name').innerText = placeName;
+           document.getElementById('price').innerText = Price;
+
+           console.log(placeName, Price);
+           setInnerText('card-count',count);
+        });
     }
